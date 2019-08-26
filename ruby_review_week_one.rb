35b9710dfbc10b0a9ccca689218aa52510b7f0e7
Harrison Malone <harrison.malone2@gmail.com>
2019-08-26 18:52:16 +1000

    test written

---
+++ ruby_review_week_one.rb
@@ -0,0 +1,63 @@
+# You may NOT use Google. These questions are just to test where you are at. Get through as many of these as you can. If you don’t get through all of them it’s ok - they are designed to take longer than the available time. If you can’t remember a bit of code, write the pseudo-code that represents what you would write your Ruby code.
+
+# 1. Describe in words what is happening in the following code:
+
+# a.
+# this_variable = 5
+
+# b.
+# some_array = [ 1, 2, 3 ]
+
+# c.
+# other_var = "hi"
+# one_var = other_var
+
+# d.
+# counter = 0
+# count += 1
+
+# e.
+# hello = "hello there"
+# if hello != "hello"
+#   puts "hello"
+# end 
+
+# f.
+# array_name.each do | item |
+#   puts item
+# end 
+
+# g.
+# while counter < 150 do
+#   puts "hello"
+# end 
+
+# 2. Use string interpolation and the variable my_string = "Fred" to print to output the string "My name is Fred"
+
+# 3. Give one method to remove whitespace from user input in Ruby
+
+# 4. What method would you use to get input from a user?
+
+# 5. What result would you get in pry or IRB for this equation: 2 + 20 / 7 ?
+
+# 6. What result would you get in pry or IRB for this equation: 2 + 7 % 3 ?
+
+# 7. Assign an empty string to a variable of any name
+
+# 8. Describe two ways you can loop through an array?
+
+# 9. How could you put the number 6 on the end of the array number_array?
+
+# 11. What name do we give to the Ruby objects denoted by true and false?
+
+# 12. Give a brief description of coercion in Ruby with an example?
+
+# 13. What does Ruby coerce nil to in an if statement?
+
+# 14.  Assign the string “hello” to a variable called some_var
+
+# 15. Take the variable from Question 14, and use an inbuilt Ruby method to turn it into an array where each character in the string is now an element in the array.
+
+# 16.  Using that array that you have created (or assume you have an array of single characters), make a loop and in that loop print out each letter from the array.
+
+# 17. Name one method you could use to access the last element in an array 
