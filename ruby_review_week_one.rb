bfdcaff4528ee2e81e1e883ebe7fd7483b90322f
Tom <asktomhunter@gmail.com>
2019-08-28 12:04:57 +1000

    Toms review

--- ruby_review_week_one.rb
+++ ruby_review_week_one.rb
@@ -4,61 +4,88 @@
 
 # a.
 # this_variable = 5
+# creates a variable this_variable and stores the value of 5 inside it.
 
 # b.
 # some_array = [ 1, 2, 3 ]
+# creates an array name called some_array and stores multiple values within it.
 
 # c.
 # other_var = "hi"
 # one_var = other_var
+#creates a variable other_var and stores the string "hi" within it. On line 15 one_var variable calls the variable other_var which would return as the string hi.
 
 # d.
 # counter = 0
 # counter += 1
+# creates the variable counter and assignes the value of 0. Then take the variable counter and assign it a new value of 1.
+
 
 # e.
 # hello = "hello there"
 # if hello != "hello"
 #   puts "hello"
 # end 
+# creates a variable hello and assignes it a string "hello there"
+# then creats an if statement starting with if hello equals false then print a string hello.
 
 # f.
 # some_array = [ 1, 2, 3 ]
 # some_array.each do | item |
 #   puts item
 # end 
+# create an array called some_array and stores values of 1,2 and 3 within it.
+# assignes a method .each to array some_array and stores an input of either 1 2 or 3.
+# prints item on the screen
 
 # g.
 # while counter < 150 do
 #   puts "hello"
 # end 
+# prints hello 150 times?
 
 # 2. Use string interpolation and the variable my_string = "Fred" to print to output the string "My name is Fred"
+# my_string = "Fred"
+# print "My name is #{my_string}".
 
 # 3. Give one method to remove whitespace from user input in Ruby
+# my_string.chomp
 
 # 4. What method would you use to get input from a user?
+# .gets
 
 # 5. What result would you get in pry or IRB for this equation: 2 + 20 / 7 ?
+# 16
 
 # 6. What result would you get in pry or IRB for this equation: 2 + 7 % 3 ?
+# 4
 
 # 7. Assign an empty string to a variable of any name
+# my_name = ""
 
 # 8. Describe two ways you can loop through an array?
+# Unknown
 
 # 9. How could you put the number 6 on the end of the array number_array?
+# number_array.gets.push
 
 # 11. What name do we give to the Ruby objects denoted by true and false?
+# boolean
 
 # 12. Give a brief description of coercion in Ruby with an example?
+# Unknown
 
 # 13. What does Ruby coerce nil to in an if statement?
+# Unknown
 
 # 14.  Assign the string “hello” to a variable called some_var
+# some_var = "hello"
 
 # 15. Take the variable from Question 14, and use an inbuilt Ruby method to turn it into an array where each character in the string is now an element in the array.
+# some_var.unknown
 
 # 16.  Using that array that you have created (or assume you have an array of single characters), make a loop and in that loop print out each letter from the array.
+# ?
 
-# 17. Name one method you could use to access the last element in an array 
+# 17. Name one method you could use to access the last element in an array
+# ?
